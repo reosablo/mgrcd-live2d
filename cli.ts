2911ced3c816f9d9c3edf5c1f1b3243e4a670d58
reosablo <reosablo@users.noreply.github.com>
2021-08-09 16:40:57 +0900

    add version info

--- cli.ts
+++ cli.ts
@@ -3,8 +3,12 @@ import { command as bakeExModelCommand } from "./cliffy-commands/bake-ex-model.t
 import { command as listCharaCommand } from "./cliffy-commands/list-chara.ts";
 import { command as listModelCommand } from "./cliffy-commands/list-model.ts";
 import { command as listScenarioCommand } from "./cliffy-commands/list-scenario.ts";
+import { description, name, version } from "./package.ts";
 
-const command = new Command()
+const command = new Command<void>()
+  .version(version)
+  .name(name)
+  .description(description)
   .command("bake-ex-model", bakeExModelCommand)
   .command("list-model", listModelCommand)
   .command("list-scenario", listScenarioCommand)
---
+++ package.ts
@@ -0,0 +1,6 @@
+export const version = "1.0.0";
+export const name = "mgrcd-live2d";
+export const description = `
+Live2D model generator for Live2DViewerEX from magireco game contents
+https://github.com/reosablo/mgrcd-live2d#readme
+`.trim();
