c0b637ab6cf0edb03b2ab3148ed8cdc5eec75b10
reosablo <reosablo@users.noreply.github.com>
2021-08-21 20:10:29 +0900

    v1.1.0

--- package.ts
+++ package.ts
@@ -1,4 +1,4 @@
-export const version = "1.0.1";
+export const version = "1.1.0";
 export const name = "mgrcd-live2d";
 export const description = `
 Live2D model generator for Live2DViewerEX from magireco game contents
