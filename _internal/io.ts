7b035ca87840d845d243e8fe46d6ae4dcbcd2ac6
reosablo <reosablo@users.noreply.github.com>
2021-10-27 16:48:03 +0900

    update std to 0.113.0

--- _internal/io.ts
+++ _internal/io.ts
@@ -2,7 +2,7 @@ import {
   isAbsolute as isAbsolutePath,
   join as joinPath,
   sep,
-} from "https://deno.land/std@0.100.0/path/mod.ts";
+} from "https://deno.land/std@0.113.0/path/mod.ts";
 import { parse as parseHjson } from "https://deno.land/x/hjson_deno@v1.0.1/mod.ts";
 import type { Model } from "../types/Live2DViewerEX/model.model3.ts";
 import { paramSchema } from "../zod-schemas/magireco/model-params.ts";
