454e1e7d43acf7c83b2d25b5851cc18f329d8591
reosablo <reosablo@users.noreply.github.com>
2021-10-31 18:11:08 +0900

    delete unused properties

--- types/magireco/scenario.ts
+++ types/magireco/scenario.ts
@@ -1,12 +1,10 @@
 export type Action = {
   cheek?: number;
-  effect?: "shake" | string;
   eyeClose?: number;
   face?: string;
   id?: number;
   lipSynch?: number;
   motion?: number;
-  pos?: number;
   soulGem?: number;
   tear?: number;
   textHome?: string;
@@ -16,23 +14,15 @@ export type Action = {
 };
 
 export type Scene = {
-  armatureList?: {
-    animation: ("action" | string)[];
-    armatureId: number;
-    filePath: string;
-    zOrder: "bgFront" | "messageBack" | string;
-  }[];
   autoTurnFirst?: number;
   autoTurnLast?: number;
   chara?: Action[];
-  deleteArmatureList?: number[];
   [x: string]: unknown;
 };
 
 export type Story = Scene[];
 
 export type Scenario = {
-  skipTransitionList?: unknown[];
   story?: Record<string, Story>;
   version?: number;
   [x: string]: unknown;
--- zod-schemas/magireco/scenario.ts
+++ zod-schemas/magireco/scenario.ts
@@ -2,13 +2,11 @@ import { z } from "https://deno.land/x/zod@v3.9.0/mod.ts";
 
 export const actionSchema = z.object({
   cheek: z.number(),
-  effect: z.enum(["shake"]).or(z.string()),
   eyeClose: z.number(),
   face: z.string(),
-  id: z.number().int().nonnegative(),
+  id: z.number().int(),
   lipSynch: z.number(),
-  motion: z.number().int().nonnegative(),
-  pos: z.number().int().nonnegative(),
+  motion: z.number().int(),
   soulGem: z.number(),
   tear: z.number(),
   textHome: z.string(),
@@ -17,22 +15,14 @@ export const actionSchema = z.object({
 }).partial().passthrough();
 
 export const sceneSchema = z.object({
-  armatureList: z.array(z.object({
-    animation: z.array(z.enum(["action"]).or(z.string())),
-    armatureId: z.number(),
-    filePath: z.string(),
-    zOrder: z.enum(["bgFront", "messageBack"]).or(z.string()),
-  })),
   autoTurnFirst: z.number(),
   autoTurnLast: z.number(),
   chara: z.array(actionSchema),
-  deleteArmatureList: z.array(z.number()),
 }).partial().passthrough();
 
 export const storySchema = z.array(sceneSchema);
 
 export const scenarioSchema = z.object({
-  skipTransitionList: z.array(z.unknown()).optional(),
   story: z.object({}).catchall(storySchema),
   version: z.number(),
 }).partial().passthrough();
